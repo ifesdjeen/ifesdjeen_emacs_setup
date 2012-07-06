4e3a7c5dc5bd9c741f8284fb7664de6cd83cd7b7
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-07-06 09:58:35 +0200

    Latest changes.

--- boot.el
+++ boot.el
@@ -26,3 +26,11 @@
 (load-initializer "keyboard")
 (load-initializer "ui")
 
+(add-to-list 'safe-local-eval-forms '(clojure-swank-command . "lein2 jack-in %s"))
+
+(defun kill-other-buffers ()
+    "Kill all other buffers."
+    (interactive)
+    (mapc 'kill-buffer
+          (delq (current-buffer)
+                (remove-if-not 'buffer-file-name (buffer-list)))))
