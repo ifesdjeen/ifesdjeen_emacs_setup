f22298e2f9e39f5939af29c3e886ad5e1712053d
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-08-10 11:18:23 +0200

    Updating git-emacs mode. Updating git-blame to display more complete information about the commit.

--- bundles/git/init.el
+++ bundles/git/init.el
@@ -1,2 +1,6 @@
 (push "/usr/local/bin/git" exec-path)
 (require 'git-emacs-autoloads)
+
+(require 'git-blame)
+(autoload 'git-blame-mode "git-blame"
+  "Minor mode for incremental blame for Git." t)
